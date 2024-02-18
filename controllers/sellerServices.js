088fb446a4b5dd292355d5a42808befe567cee11
vishal-sachan <vishal2010073@ssn.edu.in>
2024-02-19 00:33:10 +0530

    seller service implementations and corrections

--- controllers/sellerServices.js
+++ controllers/sellerServices.js
@@ -1,21 +1,23 @@
-const contract = require('../controllers/contract')
-const ipfs = require('../services/ipfs')
+const {contract} = require('../controllers/contract')
+const ipfs = require('../services/ipfs.js')
 const crypto = require('crypto');
 
 module.exports.storeProduct = async(req, res) => {
     console.log("Storing product details...", req.body);
     const id = crypto.randomBytes(16).toString('hex');
-    // const productDataToIPFS = {
-    //     ...req.body, 
-    //     images: req.body.
-    // }
+    const productId = id;
+    const {sellerId} = req.body;
+    
+    const formattedProductData = {
+        ...req.body,
+        productImages: req.body.productImages.map((imageObject) => {
+          return imageObject.thumbUrl;
+        }),
+      };
+
     const options = {
         pinataMetadata: {
             name: id,
-            // keyvalues: {
-            //     customKey: 'customValue',
-            //     customKey2: 'customValue2'
-            // }
         },
         pinataOptions: {
             cidVersion: 0
@@ -23,17 +25,15 @@ module.exports.storeProduct = async(req, res) => {
     };
 
     try {
-
-        const result = await ipfs.pinJSONToIPFS(req.body, options);
-
-        const productId = id;
-        const productHash = result.IpfsHash;
-        const sellerId = req.body.sellerId;
-
+        const result = await ipfs.pinJSONToIPFS(formattedProductData, options);
+        
+        const productHash = result.IpfsHash.toString();
+        
+        console.log(productId, sellerId, productHash);
         const status = await contract.storeProductInStorage(productId, sellerId, productHash);
         res.status(201).json({status: status});
     } catch(error) {
-        res.status(500).json({message: error});
+        res.status(500).json({message: error.toString()});
     }
 } 
 
@@ -41,10 +41,18 @@ module.exports.removeProductByProductId = async(req, res) => {
     const {sellerId, productId} = req.params;
 
     try {
-        await contract.removeProductFromStorage(productId, sellerId);
-        res.status(200);
+        // Removing product from ipfs
+        const productInfo = await contract.getProductByProductId(productId);
+        const ipfsHash = productInfo.ipfsHash;
+        console.log("Hash to unpin : ",ipfsHash);
+
+        const unpinStatus = await ipfs.unpinDataFromIPFS(ipfsHash);
+        console.log(`Status from Ipfs : `, unpinStatus);
+
+        const status = await contract.removeProductFromStorage(productId, sellerId);
+        res.status(200).json(status);
     } catch(error) {
-        res.status(500).json({message: error});
+        res.status(500).json({message: error.toString()});
     }
 }
 
@@ -52,10 +60,21 @@ module.exports.getAllProductBySellerId = async (req, res) => {
     const {sellerId} = req.params;
 
     try {
-        const products = await contract.getProductBySellerId(sellerId);
-        res.status(200).json(products);
+        const productList = await contract.getProductBySellerId(sellerId);
+
+        // TODO: get product data from the ipfs
+        const sellerProducts = productList.map(async ({ productId, sellerId, ipfsHash }) => {
+            const resData = await ipfs.getJSONDataFromIPFS(ipfsHash);
+            return { product: { ...resData } };
+          });
+          
+          // If you want to wait for all the promises to resolve, you can use Promise.all
+          Promise.all(sellerProducts).then((sellerProducts) => {
+            console.log("Formatted Data: ", sellerProducts);
+            return res.status(200).json({sellerProducts});
+          });
     } catch(error) {
-        res.status(500).json({message: error});
+        return res.status(500).json({message: error.toString()});
     }
 }
 
--- services/ipfs.js
+++ services/ipfs.js
@@ -1,9 +1,16 @@
 const axios = require('axios')
 const FormData = require('form-data')
 const fs = require('fs')
+// const fetch = require('node-fetch');
 const pinataSDK = require('@pinata/sdk');
 const { error } = require('console');
+const https = require('https');
 const pinata = new pinataSDK({ pinataJWTKey: `${process.env.PINATA_JWT}`});
+require('dotenv').config();
+
+const agent = new https.Agent({  
+  rejectUnauthorized: false  
+});
 
 pinata.testAuthentication().then((result) => {
   console.log(result);
@@ -21,12 +28,22 @@ module.exports.unpinDataFromIPFS = (hash) => {
     });
 }
 
-module.exports.pinJSONToIPFS = (body, options) => {
-  pinata.pinJSONToIPFS(body, options).then((result) => {
-      console.log(result);
-      return {ipfsResponse : result}
-  }).catch((err) => {
-      console.log(err);
-      throw error("IPFS service failed to upload data!");
-  });
+module.exports.pinJSONToIPFS = async(body, options) => {
+  try {
+    const result = await pinata.pinJSONToIPFS(body, options);
+    return {IpfsHash: result.IpfsHash};
+  } catch(error) {
+    throw error("IPFS service failed to upload data!");
+  }
+}
+
+module.exports.getJSONDataFromIPFS = async(IpfsHash) => {
+  try {
+    const res = await axios.get(`${process.env.PINATA_PUBLIC_GATEWAY}${IpfsHash}`, { httpsAgent: agent });
+    // const resData = await res.json();
+    console.log("Data from Ipfs: ", res.data);
+    return res.data;
+  } catch (error) {
+      console.log(error);
+  }
 }
